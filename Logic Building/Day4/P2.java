5ae2079dd4c8181f3c731c621001907cc6a9d901
Kiran Waghmare <72081819+Kiranwaghmare123@users.noreply.github.com>
2022-09-10 13:38:33 +0530

    Add files via upload

---
+++ Logic Building/Day4/P2.java
@@ -0,0 +1,21 @@
+
+class P2{
+
+public static void main(String args[]){
+	
+	for(int i=1;i<=7;i++)
+	{
+		for(int j=1;j<=i;j++)
+		{
+			System.out.print("_*$_");
+		}
+		System.out.println();
+	}
+	
+} 
+}
+
+//*$
+//*$ *$
+//*$ *$ *$ 
+
